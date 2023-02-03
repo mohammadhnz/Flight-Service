fe62f926f562e1f4be019730c602ea8cd143a2cd
Mikaeel <mikaeel.ghorbani@digikala.com>
2023-02-03 12:51:27 +0330

    Fix access and add postgres init.sql

--- backend/postgres/init.sql
+++ backend/postgres/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE project1;
 CREATE USER user1 WITH PASSWORD 'password1';
 GRANT ALL PRIVILEGES ON DATABASE "project1" to user1;
 GRANT ALL ON SCHEMA public TO user1;
+\c - user1
 
 
 CREATE TABLE IF NOT EXISTS user_account
@@ -188,3 +189,11 @@ SELECT flight.flight_id                                        AS flight_id,
 FROM flight
          JOIN aircraft_view on aircraft_view.registration = flight.aircraft;
 
+
+\COPY aircraft_type (type_id,manufacturer,model,series) FROM '/var/lib/postgresql/csvs/aircraft_type.csv' DELIMITER ',' CSV HEADER;   
+\COPY aircraft_layout (layout_id,type_id,y_class_capacity,j_class_capacity,f_class_capacity) FROM '/var/lib/postgresql/csvs/aircraft_layout.csv' DELIMITER ',' CSV HEADER;         
+\COPY aircraft (registration,layout_id) FROM '/var/lib/postgresql/csvs/aircraft.csv' DELIMITER ',' CSV HEADER;
+\COPY country (country_name) FROM '/var/lib/postgresql/csvs/country.csv' DELIMITER ',' CSV HEADER;
+\COPY city (country_name,city_name,timezone_name) FROM '/var/lib/postgresql/csvs/city.csv' DELIMITER ',' CSV HEADER;
+\COPY airport (country_name,city_name,airport_name,iata_code) FROM '/var/lib/postgresql/csvs/airport.csv' DELIMITER ',' CSV HEADER;
+\COPY flight (flight_serial,flight_id,origin,destination,aircraft,departure_utc,duration,y_price,j_price,f_price) FROM '/var/lib/postgresql/csvs/flight.csv' DELIMITER ',' CSV HEADER;

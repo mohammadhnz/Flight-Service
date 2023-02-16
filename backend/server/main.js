88f7f9ca3685f5b07a59ec509e17ba2c2a4f2279
Mikaeel <mikaeel.ghorbani@digikala.com>
2023-02-17 01:40:27 +0330

    fix: wrong log

--- backend/server/main.js
+++ backend/server/main.js
@@ -20,6 +20,6 @@ app.use(validatorMiddleware);
 
 app.listen(8081, () => {
   const blue = chalk.blue
-  const target = blue(`http://localhost:8081`)
+  const target = blue(`http://server:8081`)
   console.log(`🚀 Server ready at ${target}`)
 })
